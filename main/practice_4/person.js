652116bbb647f09a2a0e94041748b9fb6da5898d
e <wlx100715@163.com>
2018-12-22 16:24:39 +0800

    Complete some practice.

--- main/practice_4/person.js
+++ main/practice_4/person.js
@@ -1,1 +1,15 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.basic_introduce = function () {
+      return `My name is ${this.name}. I am ${this.age} years old.`; 
+    }
+  }
+  introduce() {
+    return this.basic_introduce();
+  }
+}
+module.exports = Person;
+
 
--- main/practice_4/worker.js
+++ main/practice_4/worker.js
@@ -1,1 +1,12 @@
+import Person from "./person";
+class Worker extends Person{
+  constructor(name, age) {
+    super(name,age);
+  }
+  introduce() {
+    return `${this.basic_introduce()} I am a Worker. I have a job.`;
+  }  
+}
+module.exports = Worker;
+
 
