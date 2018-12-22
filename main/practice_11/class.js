1cf077711074770b4ef51edff5b43a298a8db716
e <wlx100715@163.com>
2018-12-23 00:20:18 +0800

    Complete practice_11.

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,32 @@
+class Class {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+  assignLeader(student) {
+    if (student.klass.number === this.number) {
+      this.leader = student;
+      if (this.teacher) {
+        console.log(`I am ${this.teacher.name}. I know ${this.leader.name} become Leader of Class ${this.number}.`)
+      }
+    } else {
+      console.log("It is not one of us.");
+    }
+  }
+  appendMember(student) {
+    student.klass = this;
+    if (this.teacher) {
+      console.log(`I am ${this.teacher.name}. I know ${student.name} has joined Class ${this.number}.`)
+    }
+  }
+  registerAssignLeaderListener(teacher) {
+    this.teacher = teacher;
+  }
+  registerJoinListener(teacher) {
+    this.teacher = teacher;
+  }
+}
+module.exports = Class;
 
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,27 @@
-
+import Person from "./person";
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+  introduce() {
+    if (this.klasses && this.klasses.length) {
+      let classes = this.klasses.map(element => element.number);
+      return `${super.introduce()} I am a Teacher. I teach Class ${classes.join(', ')}.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach No Class.`
+  }
+  introduceWith(student) {
+    if (student.klass.number === this.klass.number) {
+      return `${super.introduce()} I am a Teacher. I teach ${student.name}.`
+    }
+    return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`
+  }
+  isTeaching(student) {
+    return this.klasses.some(klass => isIn(student, klass));
+  }
+  isIn(student, klass) {
+    return student.klass === klass;
+  }
+}
+module.exports = Teacher;
